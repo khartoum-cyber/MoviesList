47fcde98c84d399d206b49f7e5f1335c59d760c8
bartosz0 <methkill@gmail.com>
2021-01-17 20:40:56 +0100

    Added controllers and connected app to db

---
+++ Data/MoviesListContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesList.Models;
+
+namespace MoviesList.Data
+{
+    public class MoviesListContext : DbContext
+    {
+        public MoviesListContext(DbContextOptions<MoviesListContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
