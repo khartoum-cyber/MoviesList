697d4805a8e773365aba9d3d719fcf77efd7269a
bartosz0 <methkill@gmail.com>
2021-01-20 21:58:38 +0100

    Added new field

--- Migrations/20210120205646_Rating2.Designer.cs
+++ Migrations/20210120205646_Rating2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MoviesList.Data;
 
 namespace MoviesList.Migrations
 {
     [DbContext(typeof(MoviesListContext))]
-    partial class MoviesListContextModelSnapshot : ModelSnapshot
+    [Migration("20210120205646_Rating2")]
+    partial class Rating2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -17,5 +17,6 @@ namespace MoviesList.Models
         
         [Column(TypeName ="decimal(18, 2)")]
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 }
